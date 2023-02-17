f8e4921ad01339f4cecf975a7709cd0b63a6cb84
crownogu1 <crownuche@gmail.com>
2023-02-17 06:38:23 -0800

    my 101 quote commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio>
+#include<unistd.h>
+/**
+ * main - Entry point
+ *
+ * return: Always 1 (success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
+}
