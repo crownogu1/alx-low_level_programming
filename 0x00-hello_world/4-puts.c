7fa7b6cc8e94490ecad58de14999160b0c161ce4
crownogu1 <crownuche@gmail.com>
2023-02-16 18:04:25 -0800

    my puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include <studio.h>
+#include<stdio.h>
 /**
- * main - Entry point
- *
+ * main - entry point
+ * 
  * Return Always 0 (Success)
  */
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+
+		return (2)
 }
