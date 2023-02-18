7c906e9b39278ed83b16e111bbce4a2c326ed6b5
crownogu1 <crownuche@gmail.com>
2023-02-18 02:32:26 -0800

    my last digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,9 +4,8 @@
 
 /**
  * main - Prints the last digit of a randomly generated number
- *        and whether it is greater than 5, less than 6, or 0.
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
@@ -31,5 +30,5 @@ int main(void)
 			n, n % 10);
 	}
 
-	return (0);
-
+		return (0);
+}
