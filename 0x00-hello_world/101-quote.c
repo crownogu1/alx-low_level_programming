d7f9cd8d847e5a17f04fd83746b1670c144615e1
crownogu1 <crownuche@gmail.com>
2023-02-18 23:32:24 -0800

    my quote line commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
-#include <unistd.h>
-
+#include<unistd.h>
+#include<string.h>
 /**
- * main - this is the main function
- * description : this function writes to the standard error output stream
- * return : it returns an error 1
+ * main - This is the main Function
+ * Description: This function writes to the standard error output stream
+ * Return: It returns an error (1)`
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, text, sizeof(char) * strlen(text));
+
+	return (1);
 }
