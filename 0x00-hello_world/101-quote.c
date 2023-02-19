419a31dc77ce7495a3c5ca477ef882586db0e151
crownogu1 <crownuche@gmail.com>
2023-02-19 02:19:47 -0800

    my 101 quote commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
-#include<unistd.h>
-#include<string.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - This is the main Function
- * Description: This function writes to the standard error output stream
- * Return: It returns an error (1)`
- */
+*main - Entry point
+*Return: Always 1
+*/
+
 int main(void)
 {
-	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(STDERR_FILENO, text, sizeof(char) * strlen(text));
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
