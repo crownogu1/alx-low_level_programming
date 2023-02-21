fc4081b6dc5991204e38a1109f83253457283f54
crownogu1 <crownuche@gmail.com>
2023-02-21 07:36:45 -0800

    my print putchar commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include<main.h>
-
+#include "main.h"
 /**
- * main - this program prints putchar
+ *main - Main function prits putchar
  *
- * Return: Always 0 (success)
+ * Return: No return
  */
 int main(void)
 {
-	putchar("_");
-	putchar("p");
-	putchar("u");
-	putchar("t");
-	putchar("c");
-	putchar("h");
-	putchar("a");
-	putchar("r");
-	putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
