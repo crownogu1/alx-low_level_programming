fe79448049765612150ca2a45db6c97ef0b95c28
crownogu1 <crownuche@gmail.com>
2023-02-21 07:16:06 -0800

    my header main.h commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<main.h>
+
+/**
+ * main - this program prints putchar
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
+	return (0);
+}
