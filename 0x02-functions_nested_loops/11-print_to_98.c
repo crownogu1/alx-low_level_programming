5e69f4c76f5212fce6d7082fe175886ae99f4036
crownogu1 <crownuche@gmail.com>
2023-02-22 01:47:04 -0800

    my print to 98 commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print from n to 98
+ * @n : number to start from
+ * Return:0 or 1
+ */
+int main(void)
+
+void print_to_98(int n)
+{
+
+while (n < 98)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
+	printf("%i, ", n);
+	n++;
+}
+while (n > 98)
+{
+	printf("%i, ", n);
+	n--;
+}
+
+printf("98");
+putchar('\n');
+}
