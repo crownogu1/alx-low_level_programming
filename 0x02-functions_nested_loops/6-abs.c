0b5aee9d602694d32d3cebef15a2f6b0d5027416
crownogu1 <crownuche@gmail.com>
2023-02-21 23:59:24 -0800

    my absolute value commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @n - the number to be used in the function
+ * @n: the number to be used in the function
+ * Description: this function returns the absoulte
+ * value of a number(n) passed to it
+ * Return: int - absolute value
+ */
+	int _abs(int n)
+	{
+		if (n >= 0)
+		{
+			return (n);
+		}
+		return (-n);
+	}
