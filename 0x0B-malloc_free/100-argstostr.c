5e1888529ae592f40e49ef18ede63a3fd9cbce5a
crownogu1 <uche4dc@gmail.com>
2023-03-13 14:05:37 -0700

    my concatenate all arguments commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,76 +10,36 @@
 */
 char *argstostr(int ac, char **av)
 {
-	char *new, *salt;
-	int i, j, k;
+	int i, j, k, len;
+	char *str;
 
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
-	}
-	else
-	{
-		for (i = 0, k = 0 ; i < ac ; i++, k++)
-		{
-			for (j = 0 ; av[i][j] != '\0' ; j++, k++)
-			{
-				;
-			}
-		}
-		new = malloc(sizeof(char) * (k + 1));
-		salt = "\n";
-		for (i = 0 ; i < ac ; i++)
-		{
-			new = _strcat(new, av[i]);
-			if (new == NULL)
-			{
-				return (NULL);
-			}
-			if (i + 1 < ac)
-			{
-				new = _strcat(new, salt);
-				if (new == NULL)
-				{
-					return (NULL);
-				}
-			}
-		}
 
-	}
-	new = _strcat(new, salt);
-	if (new == NULL)
+	for (i = 0; i < ac; i++)
 	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++;
+	}
+
+	str = malloc(sizeof(char) * (len + 1));
+
+	if (str == NULL)
 		return (NULL);
-	}
-	return (new);
-}
 
-/**
- * _strcat - concatenates two strings
- * @dest: input parameter string
- * @src: input parameter string
- *
- * Return: dest
- */
-char *_strcat(char *dest, char *src)
-{
-	int a;
-	int b;
+	k = 0;
 
-	a = 0;
-
-	while (dest[a] != 0)
+	for (i = 0; i < ac; i++)
 	{
-		a++;
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
 	}
-
-	b = 0;
-
-	while (src[b] != 0)
-	{
-		dest[a] = src[b];
-		a++;
-		b++;
-	}
-	return (dest);
+	
+	return (str);
 }
