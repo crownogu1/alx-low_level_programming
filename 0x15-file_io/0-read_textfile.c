10b7b8515e817372ad9d540cdc5ac9eaa9cec182
crownogu1 <uche4dc@gmail.com>
2023-04-09 18:57:28 -0700

    my function that reads text file commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,26 +13,26 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
-	char *bufr;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	bufr = malloc(sizeof(char) * letters);
-	if (bufr == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
 	o = open(filename, O_RDONLY);
-	r = read(o, bufr, letters);
-	w = write(STDOUT_FILENO, bufr, r);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(bufr);
+		free(buffer);
 		return (0);
 	}
 
-	free(bufr);
+	free(buffer);
 	close(0);
 
 	return (w);
